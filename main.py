9d8e66751be88bc2b425e9d526b6c2e88f763c1d
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2026-01-19 18:52:32 -0500

    added dimensional accuracy, changes model parameters

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ from stocks import StocksDataLoaders, StocksDataset
 
 # Initialize important variables
 RANDOM_SEED: int = 1290
-SEQUENCE_LENGTH: int = 60
+SEQUENCE_LENGTH: int = 30
 TRAIN_FRACTION: float = 0.8
 VAL_FRACTION: float = 0.1
 BATCH_SIZE: int = 256
@@ -89,7 +89,7 @@ def prepare_data(stocks_dir: Path) -> Tuple[StocksDataLoaders, StandardScaler]:
     all_test_X, all_test_y = [], []
 
     files: List[Path] = list(stocks_dir.glob("*.csv"))
-    for file in tqdm(files, desc=f"Parsing {stocks_dir}"):
+    for file in tqdm(files, desc=f"Parsing all CSVs from {stocks_dir}"):
         df: pl.DataFrame = pl.read_csv(file).drop_nulls()
         # Ensures that model has enough context to learn something
         if len(df) < SEQUENCE_LENGTH + 10:
@@ -97,6 +97,7 @@ def prepare_data(stocks_dir: Path) -> Tuple[StocksDataLoaders, StandardScaler]:
 
         processed_data: ProcessedData = make_windows_per_ticker(df)
 
+        # .size = # of elements
         if processed_data.train_X.size > 0:
             all_train_X.append(processed_data.train_X)
             all_train_y.append(processed_data.train_y)
@@ -162,7 +163,7 @@ def train_step(
     model.train()
     train_loss: float = 0
     for X_train, y_train in tqdm(
-        train_loader, desc=f"Number of Batches Left for Epoch - {epoch}"
+        train_loader, desc=f"Number of Train Batches Left for Epoch - {epoch}"
     ):
         X_train = X_train.unsqueeze(-1).to(device)
         y_train = y_train.unsqueeze(-1).to(device)
@@ -183,10 +184,11 @@ def val_step(
     scaler: StandardScaler,
     scheduler: ReduceLROnPlateau,
     epoch: int,
-) -> float:
+) -> Tuple[float, float]:
     model.eval()
-    val_predictions: List = []
-    val_targets: List = []
+    val_predictions: List[torch.Tensor] = []
+    val_targets: List[torch.Tensor] = []
+    dim_accuracy: float = 0.0
     with torch.no_grad():
         for X_val, y_val in tqdm(
             val_loader, desc=f"Number of Val Batches Left for Epoch - {epoch}"
@@ -207,17 +209,22 @@ def val_step(
     )
     final_targets: np.ndarray = scaler.inverse_transform(final_targets_scaled.numpy())
 
+    dim_correct: np.ndarray = np.sign(final_predictions) == np.sign(final_targets)
+    dim_accuracy = np.mean(dim_correct).item()
+
     val_rmse: float = root_mean_squared_error(final_targets, final_predictions)
 
     scheduler.step(val_rmse)
 
-    return val_rmse
+    return val_rmse, dim_accuracy
 
 
-def plot_model_performance(all_losses: List[float], all_rsme: List[float]) -> None:
+def plot_model_performance(
+    all_losses: List[float], all_rsme: List[float], all_dim_accuracies: List[float]
+) -> None:
     print("Saving model performance...")
 
-    fig, ax1 = plt.subplots(figsize=(10, 6))
+    fig, (ax1, ax3) = plt.subplots(2, 1, figsize=(10, 10), sharex=True)
 
     color = "tab:blue"
     ax1.set_xlabel("Epochs")
@@ -230,8 +237,16 @@ def plot_model_performance(all_losses: List[float], all_rsme: List[float]) -> No
     ax2.set_ylabel("Val RMSE (Log Returns)", color=color)
     ax2.plot(all_rsme, color=color, label="Val RMSE")
     ax2.tick_params(axis="y", labelcolor=color)
+    ax1.set_title("Loss and Error Over Epochs")
 
-    plt.title("Model Performance Over Epochs")
+    ax3.plot(all_dim_accuracies, color="tab:green", label="Directional Accuracy")
+    ax3.axhline(
+        y=0.50, color="black", linestyle="--", alpha=0.5, label="Random Guess (50%)"
+    )
+    ax3.set_xlabel("Epochs")
+    ax3.set_ylabel("Accuracy (%)")
+    ax3.set_title("Model Directional Accuracy")
+
     fig.tight_layout()
     plt.savefig(MODEL_INFO_DIR / "model_performance.png")
 
@@ -246,7 +261,7 @@ def main() -> None:
     data_loaders, scaler = prepare_data(info.stocks_directory)
 
     model: StockPredictionModel = StockPredictionModel(
-        input_dim=1, hidden_dim=32, num_layers=2, output_dim=1, device=device
+        input_dim=1, hidden_dim=128, num_layers=2, output_dim=1, device=device
     ).to(device)
     criterion: nn.MSELoss = nn.MSELoss().to(device)
     optimizer: optim.Adam = optim.Adam(model.parameters(), lr=LEARNING_RATE)
@@ -259,17 +274,19 @@ def main() -> None:
 
     all_losses: List[float] = []
     all_rmse: List[float] = []
+    all_dim_acccuracies: List[float] = []
     for epoch in tqdm(range(EPOCHS), desc="Number of Epochs Left"):
         losses: float = train_step(
             model, data_loaders.train, criterion, optimizer, epoch
         )
         all_losses.append(losses)
 
-        rsme: float = val_step(model, data_loaders.val, scaler, scheduler, epoch)
+        rsme, dim_accuray = val_step(model, data_loaders.val, scaler, scheduler, epoch)
         all_rmse.append(rsme)
+        all_dim_acccuracies.append(dim_accuray)
 
         torch.save(model.state_dict(), model_save_dir / f"model_{epoch}.pt")
-    plot_model_performance(all_losses, all_rmse)
+    plot_model_performance(all_losses, all_rmse, all_dim_acccuracies)
     print("Model training complete!")
 
 
